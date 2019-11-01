e041779181139686ffad8f97d39e132e5f2ca701
RenatoYuzo <renatoyuzo93@gmail.com>
2019-11-01 10:52:12 -0400

    Seção 4: Projeto - Aula 60.

--- src/components/book/BookListPage.vue
+++ src/components/book/BookListPage.vue
@@ -7,6 +7,10 @@
     </v-row>
 
     <v-row>
+      <v-col cols="12">
+        <v-text-field v-model="textSearch" label="Pesquise algo..." @change="onSearch"></v-text-field>
+      </v-col>
+
       <v-col v-for="(book, i) in booklist" :key="i" cols="12" md="3" lg="2">
         <v-card class="mx-auto">
           <v-card-title>{{book.volumeInfo.title}}</v-card-title>
@@ -28,16 +32,39 @@ export default {
   name: "BookListPage",
   data() {
     return {
-      booklist: []
+      booklist: [],
+      textSearch: ""
     };
   },
-  created() {
-    axios
-      .get("https://www.googleapis.com/books/v1/volumes?q=flowers")
-      .then(response => {
-        this.booklist = response.data.items;
-      });
+  // watch: {
+  //   textSearch() {
+  //     axios
+  //       .get("https://www.googleapis.com/books/v1/volumes?q=" + this.textSearch)
+  //       .then(response => {
+  //         this.booklist = response.data.items;
+  //       });
+  //   }
+  // },
+  methods: {
+    onSearch() {
+      if (this.textSearch) {
+        axios
+          .get(
+            `https://www.googleapis.com/books/v1/volumes?q=" + $(this.textSearch)`
+          )
+          .then(response => {
+            this.booklist = response.data.items;
+          });
+      }
+    }
   }
+  // created() {
+  //   axios
+  //     .get("https://www.googleapis.com/books/v1/volumes?q=flowers")
+  //     .then(response => {
+  //       this.booklist = response.data.items;
+  //     });
+  // }
 };
 </script>
 
